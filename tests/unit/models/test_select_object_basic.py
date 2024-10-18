e97ca66825a54f62ec51a0d85958a27604aec756
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2024-10-18 18:25:59 +0800

    Optimize code

--- tests/unit/models/test_select_object_basic.py
+++ tests/unit/models/test_select_object_basic.py
@@ -4,7 +4,7 @@ import unittest
 import xml.etree.ElementTree as ET
 from alibabacloud_oss_v2 import serde
 from alibabacloud_oss_v2.models import select_object_basic as model
-from alibabacloud_oss_v2.types import OperationInput, OperationOutput, CaseInsensitiveDict, SelectResponseAdapter
+from alibabacloud_oss_v2.types import OperationInput, OperationOutput, CaseInsensitiveDict
 from .. import MockHttpResponse
 
 
