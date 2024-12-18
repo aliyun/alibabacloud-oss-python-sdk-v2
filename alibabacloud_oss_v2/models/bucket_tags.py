43503e70cc4d72995cf169b1f5e280faaff5c629
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2024-12-18 13:26:48 +0800

    Remove unnecessary tags and TagSets, and reference the ones that already exist in object_masic

--- alibabacloud_oss_v2/models/bucket_tags.py
+++ alibabacloud_oss_v2/models/bucket_tags.py
@@ -1,63 +1,7 @@
 import datetime
 from typing import Optional, List, Any, Union
 from .. import serde
-
-
-class Tag(serde.Model):
-    """
-    The container used to store the tag that you want to configure.
-    """
-
-    _attribute_map = { 
-        'key': {'tag': 'xml', 'rename': 'Key', 'type': 'str'},
-        'value': {'tag': 'xml', 'rename': 'Value', 'type': 'str'},
-    }
-
-    _xml_map = {
-        'name': 'Tag'
-    }
-
-    def __init__(
-        self,
-        key: Optional[str] = None,
-        value: Optional[str] = None,
-        **kwargs: Any
-    ) -> None:
-        """
-        key (str, optional): The key of a tag. *   A tag key can be up to 64 bytes in length.*   A tag key cannot start with `http://`, `https://`, or `Aliyun`.*   A tag key must be UTF-8 encoded.*   A tag key cannot be left empty.
-        value (str, optional): The value of the tag that you want to add or modify. *   A tag value can be up to 128 bytes in length.*   A tag value must be UTF-8 encoded.*   The tag value can be left empty.
-        """
-        super().__init__(**kwargs)
-        self.key = key
-        self.value = value
-
-class TagSet(serde.Model):
-    """
-    The container for tags.
-    """
-
-    _attribute_map = {
-        'tags': {'tag': 'xml', 'rename': 'Tag', 'type': '[Tag]'},
-    }
-
-    _xml_map = {
-        'name': 'TagSet'
-    }
-
-    _dependency_map = {
-        'Tag': {'new': lambda: Tag()},
-    }
-
-    def __init__(
-        self,
-        tags: Optional[List[Tag]] = None,
-        **kwargs: Any
-    ) -> None:
-        """
-        tags (List[Tag], optional): The tags.
-        """
-        super().__init__(**kwargs)
-        self.tags = tags
+from .object_basic import TagSet, Tag
 
 
 class Tagging(serde.Model):
