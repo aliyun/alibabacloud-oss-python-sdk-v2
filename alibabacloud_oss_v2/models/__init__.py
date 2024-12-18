c20e03c7f651c35322c7c423c5ce3522912369b8
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2024-12-18 11:28:56 +0800

    add public access block

--- alibabacloud_oss_v2/models/__init__.py
+++ alibabacloud_oss_v2/models/__init__.py
@@ -19,5 +19,6 @@ from .bucket_replication import *
 from .bucket_referer import *
 from .bucket_worm import *
 from .bucket_request_payment import *
+from .public_access_block import *
 from .bucket_resource_group import *
 from .bucket_style import *
