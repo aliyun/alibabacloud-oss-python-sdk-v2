238a8473a717c9160f10a7e7a98f759473d53a95
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2024-12-18 16:15:16 +0800

    add bucket tags

--- alibabacloud_oss_v2/__init__.py
+++ alibabacloud_oss_v2/__init__.py
@@ -38,6 +38,7 @@ from .models.bucket_public_access_block import *
 from .models.public_access_block import *
 from .models.bucket_resource_group import *
 from .models.bucket_style import *
+from .models.bucket_tags import *
 
 from .config import Config
 from .client import Client
---
+++ alibabacloud_oss_v2/models/bucket_tags.py
@@ -0,0 +1,135 @@
+import datetime
+from typing import Optional, List, Any, Union
+from .. import serde
+from .object_basic import TagSet, Tag
+
+
+class Tagging(serde.Model):
+    """
+    The container that stores the returned tags of the bucket. If no tags are configured for the bucket, an XML message body is returned in which the Tagging element is empty.
+    """
+
+    _attribute_map = { 
+        'tag_set': {'tag': 'xml', 'rename': 'TagSet', 'type': 'TagSet'},
+    }
+
+    _xml_map = {
+        'name': 'Tagging'
+    }
+
+    _dependency_map = { 
+        'TagSet': {'new': lambda: TagSet()},
+    }
+
+    def __init__(
+        self,
+        tag_set: Optional[TagSet] = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        tag_set (TagSet, optional): The container that stores the returned tags of the bucket.
+        """
+        super().__init__(**kwargs)
+        self.tag_set = tag_set
+
+
+class PutBucketTagsRequest(serde.RequestModel):
+    """
+    The request for the PutBucketTags operation.
+    """
+
+    _attribute_map = { 
+        'bucket': {'tag': 'input', 'position': 'host', 'rename': 'bucket', 'type': 'str', 'required': True},
+        'tagging': {'tag': 'input', 'position': 'body', 'rename': 'Tagging', 'type': 'xml'},
+    }
+
+    def __init__(
+        self,
+        bucket: str = None,
+        tagging: Optional[Tagging] = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        bucket (str, required): The name of the bucket.
+        tagging (Tagging, optional): The request body schema.
+        """
+        super().__init__(**kwargs)
+        self.bucket = bucket
+        self.tagging = tagging
+
+
+class PutBucketTagsResult(serde.ResultModel):
+    """
+    The request for the PutBucketTags operation.
+    """
+
+class GetBucketTagsRequest(serde.RequestModel):
+    """
+    The request for the GetBucketTags operation.
+    """
+
+    _attribute_map = { 
+        'bucket': {'tag': 'input', 'position': 'host', 'rename': 'bucket', 'type': 'str', 'required': True},
+    }
+
+    def __init__(
+        self,
+        bucket: str = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        bucket (str, required): The name of the bucket.
+        """
+        super().__init__(**kwargs)
+        self.bucket = bucket
+
+
+class GetBucketTagsResult(serde.ResultModel):
+    """
+    The request for the GetBucketTags operation.
+    """
+
+    _attribute_map = { 
+        'tagging': {'tag': 'output', 'position': 'body', 'rename': 'Tagging', 'type': 'Tagging,xml'},
+    }
+
+    _dependency_map = { 
+        'Tagging': {'new': lambda: Tagging()},
+    }
+
+    def __init__(
+        self,
+        tagging: Optional[Tagging] = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        tagging (Tagging, optional): The container that stores the returned tags of the bucket. If no tags are configured for the bucket, an XML message body is returned in which the Tagging element is empty.
+        """
+        super().__init__(**kwargs)
+        self.tagging = tagging
+
+class DeleteBucketTagsRequest(serde.RequestModel):
+    """
+    The request for the DeleteBucketTags operation.
+    """
+
+    _attribute_map = { 
+        'bucket': {'tag': 'input', 'position': 'host', 'rename': 'bucket', 'type': 'str', 'required': True},
+    }
+
+    def __init__(
+        self,
+        bucket: str = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        bucket (str, required): The name of the bucket.
+        """
+        super().__init__(**kwargs)
+        self.bucket = bucket
+
+
+class DeleteBucketTagsResult(serde.ResultModel):
+    """
+    The request for the DeleteBucketTags operation.
+    """
