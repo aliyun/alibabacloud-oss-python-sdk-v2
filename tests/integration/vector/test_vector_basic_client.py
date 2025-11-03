40a33f8d24c1d7b386e6d32bf9840821ff2b4b57
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2025-11-03 14:33:49 +0800

    Update import path syntax in vectors test cases.

--- tests/integration/vector/test_vector_basic_client.py
+++ tests/integration/vector/test_vector_basic_client.py
@@ -2,7 +2,7 @@
 
 from ast import literal_eval
 import alibabacloud_oss_v2.vectors as oss_vectors
-from tests.integration import TestIntegrationVectors, random_short_bucket_name
+from .. import TestIntegrationVectors, random_short_bucket_name
 
 
 class TestVectorBasic(TestIntegrationVectors):
